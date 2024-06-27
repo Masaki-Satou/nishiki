d32516fea9a0855b345c53b759fef480993d3a68
masakisatou <masakisatou0907@gmail.com>
2024-06-27 15:39:47 +0900

    conponent

--- resources/views/components/user.blade.php
+++ resources/views/components/user.blade.php
@@ -19,34 +19,34 @@
         {{ $noIndex }}
 
         <!-- iPhoneでは、電話番号らしき文字列があると自動検出してリンクになってしまう？リンクにしたい場合は、href="tel:"で対応。 -->
-        <meta name="format-detection" content="telephone=no" />
+        <!-- <meta name="format-detection" content="telephone=no" /> -->
 
         <!-- ブラウザのタブに表示される画像 -->
         <link rel="icon" type="image/png" href="{{ asset('images/icon.webp') }}" />
         
         <!-- ウェブクリップアイコンはスマホのホーム画面に登録した際などに表示されます。正方形で150pxくらいで用意しましょう。背景を透明にすると、iPhoneでは背景が黒になってしまうので注意 -->
-        <link rel="apple-touch-icon" href="{{ asset('images/icon.webp') }}" />
+        <!-- <link rel="apple-touch-icon" href="{{ asset('images/icon.webp') }}" /> -->
 
         <!-- favicon/webclipicon -->
-        <meta property="og:site_name" content="{{ config('app.name', '京都錦市場') }}" />
+        <!-- <meta property="og:site_name" content="{{ config('app.name', '京都錦市場') }}" /> -->
         <!-- クリック（タップ）された時のアドレス -->
-        <meta property="og:url" content="{{ $tapUrl }}" />
+        <!-- <meta property="og:url" content="{{ $tapUrl }}" /> -->
         <!-- トップページであればwebsite、それ以外はarticleにします。 -->
-        <meta property="og:type" content="{{ $ogType }}" />
+        <!-- <meta property="og:type" content="{{ $ogType }}" /> -->
         
-        <meta property="og:title" content="{{ $title }}" />
-        <meta property="og:description" content="{{ $description }}" />
+        <!-- <meta property="og:title" content="{{ $title }}" /> -->
+        <!-- <meta property="og:description" content="{{ $description }}" /> -->
         <!-- シェアされた時に表示したい画像を設定します。絶対パスで記入します。1200px × 630pxの画像画像の中の主要なコンテンツは正方形内に納める -->
-        <meta property="og:image" content="{{ asset('/images/icon.webp') }}" />
-        <meta property="og:locale" content="ja_JP" />
+        <!-- <meta property="og:image" content="{{ asset('/images/icon.webp') }}" /> -->
+        <!-- <meta property="og:locale" content="ja_JP" /> -->
 
         <!-- 運営サイトのアカウントがある場合設定 -->
         <!-- <meta name="twitter:site" content="@moshamusha2010" /> -->
 
-        <meta name="twitter:card" content="summary" />
+        <!-- <meta name="twitter:card" content="summary" /> -->
         
-        <meta name="twitter:description" content="{{ $description }}" />
-        <meta name="twitter:image:src" content="{{ asset('/images/icon.webp') }}" />
+        <!-- <meta name="twitter:description" content="{{ $description }}" /> -->
+        <!-- <meta name="twitter:image:src" content="{{ asset('/images/icon.webp') }}" /> -->
 
 
 
